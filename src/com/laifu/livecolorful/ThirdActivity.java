8a21f78a23206688cbfefc1fa92441f1e6ac184d
dubin <dubin@dubin-Lenovo-G460.(none)>
2013-07-22 07:52:26 +0800

    second

--- src/com/laifu/livecolorful/ThirdActivity.java
+++ src/com/laifu/livecolorful/ThirdActivity.java
@@ -1,14 +1,20 @@
 package com.laifu.livecolorful;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 public class ThirdActivity extends LiveBaseActivity {
 
@@ -44,6 +50,7 @@ public class ThirdActivity extends LiveBaseActivity {
 	private SharedPreferences mPre;
 	private int mCurrentPage;
 	private static String CURRENT_PAGE = "current_page";
+	private ListView my_rount_list;
 
 	private OnClickListener mylinearListner = new LinearLayout.OnClickListener() {
 		@Override
@@ -86,12 +93,60 @@ public class ThirdActivity extends LiveBaseActivity {
 			my_info_arrow.setVisibility(android.view.View.VISIBLE);
 		}
 	}
-
+	private List<Map<String, Object>> getData() {
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+ 
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("title", "张根硕全球电视通告-中国歌迷会北京分会朝阳区分会");
+        map.put("info", "23580266个成员     67556个行程");
+        map.put("img", R.drawable.progressbar);
+        list.add(map);
+ 
+        map = new HashMap<String, Object>();
+        map.put("title", "G2");
+        map.put("info", "google 2");
+        map.put("img", R.drawable.progressbar);
+        list.add(map);
+ 
+        map = new HashMap<String, Object>();
+        map.put("title", "G3");
+        map.put("info", "google 3");
+        map.put("img", R.drawable.progressbar);
+        list.add(map);
+        
+        map = new HashMap<String, Object>();
+        map.put("title", "G3");
+        map.put("info", "google 3");
+        map.put("img", R.drawable.progressbar);
+        list.add(map);
+        
+        map = new HashMap<String, Object>();
+        map.put("title", "G3");
+        map.put("info", "google 3");
+        map.put("img", R.drawable.progressbar);
+        list.add(map);
+        
+        map = new HashMap<String, Object>();
+        map.put("title", "G3");
+        map.put("info", "google 3");
+        map.put("img", R.drawable.progressbar);
+        list.add(map);
+        return list;
+    }
+	
+	private void InitRountListView(){
+		SimpleAdapter adapter = new SimpleAdapter(this,getData(),R.layout.my_rount_list,
+                new String[]{"title","info","img"},
+                new int[]{R.id.title,R.id.info,R.id.img});
+		my_rount_list.setAdapter(adapter);
+	}
 	private void initView() {
 		mPre = PreferenceManager.getDefaultSharedPreferences(this);
 		my_rount_linear = (LinearLayout) findViewById(R.id.my_rount_linear);
 		my_rount_linear.setOnClickListener(mylinearListner);
 		my_rount_arrow = (ImageView)findViewById(R.id.my_rount_arrow);
+		my_rount_list = (ListView)findViewById(R.id.my_rount_list);
+		InitRountListView();
 
 		my_fans_linear = (LinearLayout) findViewById(R.id.my_fans_linear);
 		my_fans_linear.setOnClickListener(mylinearListner);
